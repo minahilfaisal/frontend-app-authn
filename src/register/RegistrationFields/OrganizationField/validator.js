351146530418cda944004cb829a2c3e81ae1b7dc
Minahil Faisal <minahil.faisal@arbisoft.com>
2023-10-30 18:09:00 +0500

    fix: validator file

--- src/register/RegistrationFields/OrganizationField/validator.js
+++ src/register/RegistrationFields/OrganizationField/validator.js
@@ -6,22 +6,8 @@ const validateOrganizationField = (value, organizationList, errorMessage) => {
   let displayValue = value;
   let error = errorMessage;
 
-  if (value) {
-    const normalizedValue = value.toLowerCase();
-    // Handling a case here where user enters a valid organization code that needs to be
-    // evaluated and set its value as a valid value.
-    const selectedOrganization = organizationList.find(
-      (organization) => (
-        // When translations are applied, extra space added in organization value, so we should trim that.
-        organization[ORGANIZATION_DISPLAY_KEY].toLowerCase().trim() === normalizedValue
-        || organization[ORGANIZATION_CODE_KEY].toLowerCase().trim() === normalizedValue
-      ),
-    );
-    if (selectedOrganization) {
-      organizationCode = selectedOrganization[ORGANIZATION_CODE_KEY];
-      displayValue = selectedOrganization[ORGANIZATION_DISPLAY_KEY];
-      error = '';
-    }
+  if (displayValue.trim()) { // if value is not empty, remove error
+    error = '';
   }
   return { error, organizationCode, displayValue };
 };
